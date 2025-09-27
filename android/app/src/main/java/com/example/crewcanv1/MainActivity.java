44ca0010be0db110eee50059af9ddf84cd915f02
Deepak Yadav <deepakyadav3553@gmail.com>
2025-09-27 11:30:08 +0530

    first commit

---
+++ android/app/src/main/java/com/example/crewcanv1/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.crewcanv1;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.crewcanv1',
+  appName: 'crewcanV1',
+  webDir: 'dist/crewcanV1/browser'
+};
+
+export default config;
